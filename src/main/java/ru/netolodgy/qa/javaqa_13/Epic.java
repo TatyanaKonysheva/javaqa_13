a797b41fb00b0e957a4a0b1bcf9ac1c46c9e7594
Konysheva Tatyana <tkonyseva693@gmail.com>
2023-05-21 17:05:29 +0500

    Added code

---
+++ src/main/java/ru/netolodgy/qa/javaqa_13/Epic.java
@@ -0,0 +1,24 @@
+package ru.netolodgy.qa.javaqa_13;
+
+public class Epic extends Task {
+    protected String[] subtasks;
+
+    public Epic(int id, String[] subtasks) {
+        super(id);
+        this.subtasks = subtasks;
+    }
+
+    public String[] getSubtasks() {
+        return subtasks;
+    }
+
+    @Override
+    public boolean matches(String query) {
+        for (String subtask : subtasks) {
+            if (subtask.contains(query)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/test/java/ru/netolodgy/qa/javaqa_13/TodosTest.java
@@ -0,0 +1,59 @@
+package ru.netolodgy.qa.javaqa_13;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TodosTest {
+    @Test
+    public void shouldAddThreeTasksOfDifferentType() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask, epic, meeting};
+        Task[] actual = todos.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+    String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+    Epic epic = new Epic(55, subtasks);
+
+    Meeting meeting = new Meeting(
+            555,
+            "Выкатка 3й версии приложения",
+            "Приложение НетоБанка",
+            "Во вторник после обеда"
+    );
+
+    Todos todos = new Todos();
+
+    public void setup() {
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+    }
+
+    @Test
+    public void shouldToSearchSimpleTask() {
+        Task[] expected = {simpleTask};
+        Task[] actual = todos.search("Позвонить");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
